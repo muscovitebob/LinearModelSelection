55f14fd41d7ab9ba8e4dc47fa62b213eedb4949f
Pouckie <thomas_vanpoucke@hotmail.com>
2018-04-30 13:57:10 +0200

    bad

--- crappy code stuff.R
+++ crappy code stuff.R
@@ -2,11 +2,11 @@ prostate =get(load("prostate2.rdata"))
 
 
 
-forward_stepwise <- function(data){
+forward_stepwise <- function(dataset){
   rsq = 0
   best_predictor = NULL
-  for (i in 2:length(names(data))){
-    a <- lm(Cscore~names(data)[i])
+  for (i in 2:length(names(dataset))){
+    a <- lm(Cscore~data[,i])
     new_rsq = summary(a)$r.squared
     if (new_rsq > rsq){
       rsq = new_rsq
