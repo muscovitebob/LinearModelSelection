2a8e7ac2cee2d891ad6b9490d7e1ec2da93c0477
Pouckie <thomas_vanpoucke@hotmail.com>
2018-04-30 15:44:31 +0200

    bad

--- crappy code stuff.R
+++ crappy code stuff.R
@@ -3,17 +3,26 @@ prostate =get(load("prostate2.rdata"))
 
 
 forward_stepwise <- function(data){
-  rsq = 0
-  best_predictor = NULL
-  for (i in 2:length(names(data))){
-    a <- lm(Cscore~data[,i])
-    new_rsq = summary(a)$r.squared
-    if (new_rsq > rsq){
-      rsq = new_rsq
-      best_predictor = a
+  i_vector = c()
+  lm_vector =c()
+  for (j in 1:length(names(data))-1){
+    rsq = 0
+    best_predictor = NULL
+    i_oneloop = 0
+    for (i in 2:length(names(data))){
+      if (!is.element(i,i_vector)){
+        #Mki <- lm(Cscore~data[,i], data)
+        Mki <- lm(as.formula(paste("Cscore~", paste(i_vector, collapse = "+", paste("+", paste(i))))))
+        new_rsq = summary(Mki)$r.squared
+        if (new_rsq > rsq){
+          rsq = new_rsq
+          best_predictor = Mki
+          i_oneloop = i
+        }
+      }
     }
+    append(i_vector, i_oneloop)
   }
-  return(best_predictor)
 }
 
 forward_stepwise(prostate)
