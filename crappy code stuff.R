a408e206a453b7928a04521b4c9b552f3c03df44
Boris Shilov <21023619+borisofmuscovy@users.noreply.github.com>
2018-05-01 14:41:39 +0200

    reworked to work with formula objects directly instead of strings

--- crappy code stuff.R
+++ crappy code stuff.R
@@ -5,49 +5,48 @@ bestforward = function (data){
   bestModelList = vector(mode="list")
   p = length(names(data))
   k = 0:(p-1)
-  null = lm(data$Cscore~1, data = data)
+  nullmod = lm(as.matrix(data[1])~ 1, data = data)
   usedUpVariables = c()
   predictorData = data
   predictorData[,1] = NULL
+  bestModelList[[1]] = nullmod
   for (i in k){ #iterate over all variables, starting with null model 0 and to p-1
     # now we need to pick a subset of variables that have not yet been used
+    if (length(usedUpVariables) > 0 && usedUpVariables == names(data)){
+      break
+    }
+    
     if (length(usedUpVariables != 0)){
-      constructionData = predictorData[,,!usedUpVariables]
+      constructionData = predictorData[,-which(names(predictorData) %in% usedUpVariables)]
     } else {
       constructionData = predictorData
     }
-    # now we need to iterate as many times as there are unused variables to construct the models
+    
+    # now we need to iterate as many times as there are unused variables to construct the candidate model matrix
     modellist = vector(mode="list")
-    for (j in 1:length(constructionData)){ #iterate over all Mk+1
-      provFormulaString = paste(names(response), "~")
-      for (z in 1:j){#for the current Mk+1, create the model string
-        if (z == 1){
-          provFormulaString = paste(provFormulaString, names(constructionData)[z])
-        } else {
-          provFormulaString = paste(provFormulaString, "+", names(constructionData)[z])
-        }
-      }
-      itermodel = lm(as.formula(paste(provFormulaString)), data = data) #assigns the current Mk+1 this model
-      vars = variable.names(itermodel)
-      vars = vars[-1]
-      modellist[[j]] = itermodel #add this model to the candidate model list
-      UsedUpVariables = append(usedUpVariables, vars) #add used variable to discard pile
+    for (z in 1:length(names(constructionData))){
+      itermodel = update(bestModelList[[i + 1]], ~. + as.matrix(constructionData[z]))
+      modellist[[z]] = itermodel
+      remove(itermodel)
     }
     
     # select the best model out of this Mk+1 subset
     rSquaredList = vector(mode="list")
     for (h in 1:length(modellist)){
-      rSquaredList[[h]] = summary(modellist[[h]])$r.squared
+      rSquaredList[h] = summary(modellist[[h]])$r.squared
     }
     bestModelNumber = which.max(rSquaredList)
     bestModel = modellist[bestModelNumber]
-    bestModelList[[i+1]] = bestModel
+    bestModelList[i+2] = bestModel
+    vars = variable.names(bestModel)
+    vars = vars[-1]
+    usedUpVariables = append(usedUpVariables, vars) #add used variable to discard pile
   }
   
   #now that we have the best models of all Mk+1 subsets, pick the best best model
   BICforModels = vector(mode="list")
   for (h in 1:length(bestModelList)){
-    BICforModels[[h]] = BIC(bestModelList[[h]][[1]])
+    BICforModels[h] = BIC(bestModelList[[h]])
   }
   bestBICModelNumber = which.max(BICforModels)
   return(bestModelList[[bestBICModelNumber]])
